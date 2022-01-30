feb5722d725ff35f4e154d25234c59c71f1e4695
amano <r202003488in@jindai.jp>
2022-01-30 15:04:49 +0900

    this is version 2.

--- ex5/src/ex5/Main.java
+++ ex5/src/ex5/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
-		System.out.println("hello world");
+		System.out.println("Hello World");
 		}
 	}
 
